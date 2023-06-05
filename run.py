876315b5ca525c03a9514db15fcc5d37b33a0f7e
defaultenn <evgeniyabramov25@gmail.com>
2023-06-05 21:16:27 +0400

    classify the code

--- run.py
+++ run.py
@@ -20,149 +20,146 @@ weekdays_verbose = ['Понедельник', 'Вторник', 'Среда', '�
 # начальная неделя над чертой
 start_week = datetime(2023, 5, 1, 0, 0, 0, 0)
 
-def get_university_names():
-    return list(map(lambda x: x.split('.')[0],os.listdir('data')))
+class ScheduleHelpers:
 
-def get_groups(university_name: str) -> List[str]:
-    with io.open(f'data/{university_name}.csv', 'r', newline='', encoding='utf8') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=';')
-        data = [row for row in spamreader]
-        groups = [value[0].strip() for value in data[::9]]
-        csvfile.close()
-    return groups
-
-def get_sublists(original_list, number_of_sub_list_wanted) -> List:
-    sublists = list()
-    for sub_list_count in range(number_of_sub_list_wanted): 
-        sublists.append(original_list[sub_list_count::number_of_sub_list_wanted])
-    return sublists
-
-def get_keyboard(buttons: List[str] = None, type: str = None, extra: List[str] = []):
-    keyboard_buttons = []
-    if(buttons and type):
-        keyboard_buttons = list(
-            map(
-                lambda button_str: InlineKeyboardButton(
-                    text=button_str, 
-                    callback_data=f'{type}:{button_str}:{":".join(extra)}' if extra else f'{type}:{button_str}'
-                ), buttons
+    @staticmethod
+    def get_sublists(original_list, number_of_sub_list_wanted) -> List:
+        sublists = list()
+        for sub_list_count in range(number_of_sub_list_wanted): 
+            sublists.append(original_list[sub_list_count::number_of_sub_list_wanted])
+        return sublists
+    
+    @staticmethod
+    def get_keyboard(buttons: List[str] = None, type: str = None, extra: List[str] = []):
+        keyboard_buttons = []
+        if(buttons and type):
+            keyboard_buttons = list(
+                map(
+                    lambda button_str: InlineKeyboardButton(
+                        text=button_str, 
+                        callback_data=f'{type}:{button_str}:{":".join(extra)}' if extra else f'{type}:{button_str}'
+                    ), buttons
+                )
             )
-        )
-    if(len(extra) > 0):
-        if(type == 'period'):
-            callback_data = f'university:{extra[-2]}'
-        elif(type == 'group'):
-            callback_data = f'start'
-        elif(type == None):
-            callback_data = f'group:{":".join(reversed(extra))}'
-        extra.remove(extra[-1])
         if(len(extra) > 0):
-            callback_data += f':{":".join(extra)}'
-    else:
-        callback_data = 'start'
+            if(type == 'period'):
+                callback_data = f'university:{extra[-2]}'
+            elif(type == 'group'):
+                callback_data = f'start'
+            elif(type == None):
+                callback_data = f'group:{":".join(reversed(extra))}'
+            extra.remove(extra[-1])
+            if(len(extra) > 0):
+                callback_data += f':{":".join(extra)}'
+        else:
+            callback_data = 'start'
 
-    if(not type == 'university'):
-        keyboard_buttons.append(
-            InlineKeyboardButton(
-                text='Назад', 
-                callback_data=callback_data
+        if(not type == 'university'):
+            keyboard_buttons.append(
+                InlineKeyboardButton(
+                    text='Назад', 
+                    callback_data=callback_data
+                )
             )
-        )
-    return get_sublists(keyboard_buttons, 2)
+        return ScheduleHelpers.get_sublists(keyboard_buttons, 2)
 
-async def handle_start(update: Update, context: CallbackContext):
-    names = get_university_names()
-    keyboard = get_keyboard(names, 'university')
-    bot: ExtBot = context.bot
-    await bot.send_message(
-        chat_id=update.effective_chat.id,
-        text='Выберите институт:',
-        reply_markup=InlineKeyboardMarkup(keyboard)
-    )
+class ScheduleStorage:
 
-def get_week_description():
-    return 'Неделя под чертой' if isOver() else 'Неделя над чертой'
+    @staticmethod
+    def get_university_names():
+        return list(map(lambda x: x.split('.')[0],os.listdir('data')))
 
-async def start(update: Update, context: CallbackContext):
-    await handle_start(update, context)
+    @staticmethod
+    def get_groups(university_name: str) -> List[str]:
+        with io.open(f'data/{university_name}.csv', 'r', newline='', encoding='utf8') as csvfile:
+            spamreader = csv.reader(csvfile, delimiter=';')
+            data = [row for row in spamreader]
+            groups = [value[0].strip() for value in data[::9]]
+            csvfile.close()
+        return groups
 
-def isOver():
-    delta = datetime.now() - start_week
-    return round(math.modf(delta.days / 7)[1] % 2)
-
-def get_couple_with_week(parsable: str):
-    return parsable.split('/')[int(isOver())]
-
-def get_schedule(university: str, group: str, period: str):
-    with io.open(f'data/{university}.csv', 'r', newline='', encoding='utf8') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=';')
-        data = [row for row in spamreader]
-        groups = [value[0].strip() for value in data[::9]]
-        group_index = groups.index(group)
-        data = data[group_index * 9 + 1:(group_index + 1) * 9]
-        csvfile.close()
-
-    weekday = datetime.now().weekday() % 7
+    @staticmethod
+    def get_week_description():
+        return 'Неделя под чертой' if ScheduleStorage.isOver() else 'Неделя над чертой'
     
-    if(period == 'Сегодня'):
-        if(weekday == 6):
-            return 'Отдыхай!'
-        return [
-            weekdays_verbose[weekday],
-            *[f'{index + 1}. {get_couple_with_week(row[weekday + 1])}' for index, row in enumerate(data)],
-            get_week_description()
-        ]
-    elif(period == 'Завтра'):
-        if((weekday + 1) == 6):
-            return f'Отдыхай! {weekday}'
-        return [
-            weekdays_verbose[weekday + 1],
-            *[f'{index + 1}. {get_couple_with_week(row[weekday + 1])}' for index, row in enumerate(data)],
-            get_week_description()
-        ]
-    elif(period == 'На неделю'):
-        result = []
-        for day in range(6):
-            result = [
-                *result,
-                *[
-                    weekdays_verbose[day], 
-                    *[f'{index + 1}. {get_couple_with_week(row[day])}' for index, row in enumerate(data)],
-                    '\n'
-                ]
+    @staticmethod
+    def get_couple_with_week(parsable: str):
+        return parsable.split('/')[int(ScheduleStorage.isOver())]
+
+    @staticmethod
+    def get_schedule(university: str, group: str, period: str):
+        with io.open(f'data/{university}.csv', 'r', newline='', encoding='utf8') as csvfile:
+            spamreader = csv.reader(csvfile, delimiter=';')
+            data = [row for row in spamreader]
+            groups = [value[0].strip() for value in data[::9]]
+            group_index = groups.index(group)
+            data = data[group_index * 9 + 1:(group_index + 1) * 9]
+            csvfile.close()
+
+        weekday = datetime.now().weekday() % 7
+        
+        if(period == 'Сегодня'):
+            if(weekday == 6):
+                return 'Отдыхай!'
+            return [
+                weekdays_verbose[weekday],
+                *[f'{index + 1}. {ScheduleStorage.get_couple_with_week(row[weekday + 1])}' for index, row in enumerate(data)],
+                ScheduleStorage.get_week_description()
             ]
-        result.append(get_week_description())
-        return result
-    else:
-        return []
-
-
-
-async def handle(update: Update, context: CallbackContext):
-    query = update.callback_query
-    query_data = query.data
-    type = query_data.split(":")[0]
-
-    try:
-        data = query_data.split(":")[1]
-        await query.answer()
-        await query.edit_message_text(text=f'Выбран: {data}')
-    except:
-        await query.delete_message()
-        return await handle_start(update, context)
-
+        elif(period == 'Завтра'):
+            if((weekday + 1) == 6):
+                return f'Отдыхай! {weekday}'
+            return [
+                weekdays_verbose[weekday + 1],
+                *[f'{index + 1}. {ScheduleStorage.get_couple_with_week(row[weekday + 1])}' for index, row in enumerate(data)],
+                ScheduleStorage.get_week_description()
+            ]
+        elif(period == 'На неделю'):
+            result = []
+            for day in range(6):
+                result = [
+                    *result,
+                    *[
+                        weekdays_verbose[day], 
+                        *[f'{index + 1}. {ScheduleStorage.get_couple_with_week(row[day])}' for index, row in enumerate(data)],
+                        '\n'
+                    ]
+                ]
+            result.append(ScheduleStorage.get_week_description())
+            return result
+        else:
+            return []
     
-    async def handle_university_type():
+    @staticmethod
+    def isOver():
+        delta = datetime.now() - start_week
+        return round(math.modf(delta.days / 7)[1] % 2)
+
+class ScheduleBot:
+
+    @staticmethod
+    async def handle_start(update: Update, context: CallbackContext):
+        names = ScheduleStorage.get_university_names()
+        keyboard = ScheduleHelpers.get_keyboard(names, 'university')
+        bot: ExtBot = context.bot
+        await bot.send_message(
+            chat_id=update.effective_chat.id,
+            text='Выберите институт:',
+            reply_markup=InlineKeyboardMarkup(keyboard)
+        )
+
+    @staticmethod
+    async def handle_university_type(data: str, context: CallbackContext, update: Update):
         # university:институт3
-        groups = get_groups(data)
+        groups = ScheduleStorage.get_groups(data)
         if(len(groups) == 0):
-            keyboard = get_keyboard()
+            keyboard = ScheduleHelpers.get_keyboard()
             return await context.bot.send_message(
                 chat_id=update.effective_chat.id, 
                 text="У университета нет групп.",
                 reply_markup=InlineKeyboardMarkup(keyboard)
             )
-        keyboard = get_keyboard(
+        keyboard = ScheduleHelpers.get_keyboard(
             groups, 
             'group',
             [data]
@@ -172,10 +169,12 @@ async def handle(update: Update, context: CallbackContext):
             text="Выберите группу:",
             reply_markup=InlineKeyboardMarkup(keyboard)
         )
-    async def handle_group_type():
+
+    @staticmethod
+    async def handle_group_type(query_data: str, data: str, context: CallbackContext, update: Update):
         # group:20-191-1:институт3
         university = query_data.split(':')[2]
-        keyboard = get_keyboard([
+        keyboard = ScheduleHelpers.get_keyboard([
                 'Сегодня',
                 'Завтра',
                 'На неделю'
@@ -188,34 +187,49 @@ async def handle(update: Update, context: CallbackContext):
             text="Выберите период:",
             reply_markup=InlineKeyboardMarkup(keyboard)
         )
-    async def handle_period_type():
+
+    @staticmethod
+    async def handle_period_type(query_data: str, data: str, context: CallbackContext, update: Update):
         # period:Сегодня:институт3:20-191-1
         university = query_data.split(':')[2]
         group = query_data.split(':')[3]
-        schedule = get_schedule(university, group, data)
+        schedule = ScheduleStorage.get_schedule(university, group, data)
         if(isinstance(schedule, list)):
             schedule = '\n'.join(schedule)
-        keyboard = get_keyboard(extra=[university, group])
+        keyboard = ScheduleHelpers.get_keyboard(extra=[university, group])
         await context.bot.send_message(
             chat_id=update.effective_chat.id, 
             text=schedule,
             reply_markup=InlineKeyboardMarkup(keyboard)
         )
 
-    if(type == 'university'):
-        await handle_university_type()
-    elif(type == 'group'):
-        await handle_group_type()
-    elif(type == 'period'):
-        await handle_period_type()
-        
-    else:
-        await context.bot.send_message(
-            chat_id=update.effective_chat.id, 
-            text="Я не понимаю команду, что Вы от меня хотите."
-        )
+    @staticmethod
+    async def handle(update: Update, context: CallbackContext):
+        query = update.callback_query
+        query_data = query.data
+        type = query_data.split(":")[0]
+
+        try:
+            data = query_data.split(":")[1]
+            await query.answer()
+            await query.edit_message_text(text=f'Выбран: {data}')
+        except:
+            await query.delete_message()
+            return await ScheduleBot.handle_start(update, context)
+
+        if(type == 'university'):
+            await ScheduleBot.handle_university_type(data, context, update)
+        elif(type == 'group'):
+            await ScheduleBot.handle_group_type(query_data, data, context, update)
+        elif(type == 'period'):
+            await ScheduleBot.handle_period_type(query_data, data, context, update)
+        else:
+            await context.bot.send_message(
+                chat_id=update.effective_chat.id, 
+                text="Я не понимаю команду, что Вы от меня хотите."
+            )
 
 app = ApplicationBuilder().token(bot_token).build()
-app.add_handler(CommandHandler('start', start))
-app.add_handler(CallbackQueryHandler(handle))
+app.add_handler(CommandHandler('start', ScheduleBot.handle_start))
+app.add_handler(CallbackQueryHandler(ScheduleBot.handle))
 app.run_polling()
